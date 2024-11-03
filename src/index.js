c5a4d3b9cc228699203e75fb5606954353205458
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2024-11-04 00:22:16 +1300

    Added routing for the project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import CreateProduct from "./Pages/CreateProduct";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
+import Product from "./Pages/Product";
+import NotFound from "./NotFound";
+import CryptoDetail from "./CryptoDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route
+          path="/cryptodetail/:cryptoSymbol/:id"
+          element={<CryptoDetail />}
+        ></Route>
+        <Route
+          path="/cryptodetail/:cryptoSymbol"
+          element={<CryptoDetail />}
+        ></Route>
+        <Route path="product">
+          <Route path="" element={<Product />}></Route>
+          <Route path="create" element={<CreateProduct />}></Route>
+          <Route path="details/:productId" element={<ProductDetails />}></Route>
+          <Route path="list" element={<ProductList />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
